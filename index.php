91721921e844dd16c1580c1484a040529d3b2699
Q-gitcode21 <qqmaytinh2023@gmail.com>
2025-04-13 23:06:56 +0700

    up index alert error

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
-<?php 
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 session_start();
-include_once'./MVC/bridge.php';
- $myapp =new app();
-?> 
+include_once './MVC/bridge.php';
+$myapp = new app();
+?>
